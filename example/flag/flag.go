756748b2cfacec70a351c283d2265838e66a5f65
costa <costa@hellotalk.com>
2022-09-24 19:39:45 +0800

    feat: 添加cli

---
+++ example/flag/flag.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+var inputName = flag.String("name", "CHENJIAN", "Input Your Name.")
+var inputFlagvar int
+
+func Init() {
+	flag.IntVar(&inputFlagvar, "flagname", 1234, "Help")
+}
+
+func main()  {
+	Init()
+	flag.Parse()
+
+	fmt.Printf("args=%s, num=%d\n", flag.Args(), flag.NArg())
+	for i := 0; i != flag.NArg(); i++ {
+		fmt.Printf("arg[%d]=%s\n", i, flag.Arg(i))
+	}
+	fmt.Println("name=", *inputName)
+}
